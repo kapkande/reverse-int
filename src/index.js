77c3876a83ae87fa73a19dde8624c6d01bf0ef1e
Po5 <Po5@dc.liftservis.by>
2022-08-10 10:51:17 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+    let arr = String(n).split('')
+   let add = []
+   for (let i = arr.length -1; i > -1; i--){
+      add.push(arr[i])
+   }
   
+let resolt = Number(add.join(''))
+        return resolt
+     
 }
